b36bdd76663596b12ea5b268138c5e5449bf6078
paulayo93 <52790286+paulayo93@users.noreply.github.com>
2024-12-04 07:33:47 +0100

    add dependencies and setup navigator

---
+++ src/navigation/MainStack.tsx
@@ -0,0 +1,17 @@
+import {createNativeStackNavigator} from  '@react-navigation/native-stack'
+import Home from './../screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+
+const MainStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="home" component={Home} />
+
+    </Stack.Navigator>
+  )
+}
+
+export default MainStack;
---
+++ src/screens/Home.tsx
@@ -0,0 +1,15 @@
+import {Text, View} from 'react-native';
+import {ms} from 'react-native-size-matters'
+
+const Home = () => {
+
+  return (
+    <View>
+      <Text style={{fontFamily: 'Satoshi-Medium', fontSize: ms(16), lineHeight: 20}}> Welcome
+      </Text>
+    </View>
+  )
+}
+
+
+export default  Home;
